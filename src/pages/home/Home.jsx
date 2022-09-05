d77bd443f203b1a99b6aae6a09a33d31b67f022c
Bob Park <hwpark11@kakao.com>
2022-09-05 22:00:19 +0900

    feat: img blob 으로 처리하기

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+const imgs = ['i1', 'i2', 'i3'];
 
 export default function Home() {
+  // const [imgSrc, setImgSrc] = useState();
+
+  useEffect(() => {
+    imgs.forEach((img) => {
+      axios
+        .get('/api/img', {
+          responseType: 'blob',
+        })
+        .then((res) => {
+          const imgUrl = URL.createObjectURL(res.data);
+
+          const imgTag = document.getElementById(img);
+          imgTag.src = imgUrl;
+
+          return imgUrl;
+        })
+        .then((url) => {
+          URL.revokeObjectURL(url);
+        });
+    });
+  }, []);
+
   return (
     <div>
-      <div> home</div>
+      <div>home</div>
+      {imgs.map((id) => (
+        <div key={id}>
+          <img id={id} />
+        </div>
+      ))}
     </div>
   );
 }
